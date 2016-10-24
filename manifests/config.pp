c011c018349d50477aa86fa4bb45aedda0afb098
Alan Crosswell <alan@crosswell.us>
2016-10-24 19:42:16 -0400

    /etc/ax25/axports

--- manifests/config.pp
+++ manifests/config.pp
@@ -16,4 +16,11 @@ class soundmodem::config inherits soundmodem {
     mode    => '0644',
     content => template('soundmodem/soundmodem.conf.erb'),
   }
+  file { '/etc/ax25/axports':
+    ensure  => file,
+    owner   => 0,
+    group   => 0,
+    mode    => '0644',
+    content => template('soundmodem/axports.erb'),
+  }
 }
